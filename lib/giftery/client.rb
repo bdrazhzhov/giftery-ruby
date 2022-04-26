cde5616e64c185550e5426764c9aa184885c72f3
Boris Drazhzhov <templar8@gmail.com>
2022-04-26 13:48:11 +0300

    Added documentation comments to the Giftery::Client class methods

--- lib/giftery/client.rb
+++ lib/giftery/client.rb
@@ -11,7 +11,7 @@ module Giftery
   OUTPUT_FORMAT = 'json'
 
   # Giftery::Client class needs to be used
-  # for making requests to Giftery API
+  # for making requests to Giftery API https://docs.giftery.tech/b2b-api/
   class Client
     def initialize(client_id, secret)
       @client_id = client_id
@@ -22,43 +22,61 @@ module Giftery
       make_request('test')
     end
 
+    # Creates order
+    # More info here: https://docs.giftery.tech/b2b-api/methods/makeOrder/
     def make_order(product_id, face, options = {})
       data = options.merge!(product_id: product_id, face: face)
       make_request('makeOrder', data)
     end
 
+    # Returns list of available products
+    # More info here: https://docs.giftery.tech/b2b-api/methods/getProducts/
     def products
       make_request('getProducts')
     end
 
+    # Returns base64 encoded PDF certificate
+    # More info here: https://docs.giftery.tech/b2b-api/methods/getCertificate/
     def certificate(queue_id)
       make_request('getCertificate', queue_id: queue_id)
     end
 
+    # Returns certificate requisites
+    # More info here: https://docs.giftery.tech/b2b-api/methods/getCode/
     def code(queue_id, order_id)
       make_request('getCode',
                    queue_id: queue_id,
                    order_id: order_id)
     end
 
+    # Returns certificate links
+    # More info here: https://docs.giftery.tech/b2b-api/methods/getLinks/
     def links(queue_id, order_id)
       make_request('getLinks',
                    queue_id: queue_id,
                    order_id: order_id)
     end
 
+    # Returns status of order that is created by make_order
+    # More info here: https://docs.giftery.tech/b2b-api/methods/getStatus/
     def status(id)
       make_request('getStatus', id: id)
     end
 
+    # Returns list of available certificate categories
+    # More info here: https://docs.giftery.tech/b2b-api/methods/getCategories/
     def categories
       make_request('getCategories')
     end
 
+    # Returns your available balance
+    # More info here: https://docs.giftery.tech/b2b-api/methods/getBalance/
     def balance
       make_request('getBalance')
     end
 
+    # Returns list of places (with address) where certificate can be accepted
+    # More info here: https://docs.giftery.tech/b2b-api/methods/getAddress/
     def address(product_id, area_name, locality_name)
       make_request('getBalance',
                    product_id: product_id,
